cf177fb722b71a1c88c6afa7e232c76e010eb6ea
Jennifer <jennijenjen9@gmail.com>
2016-06-30 17:15:28 -0700

    added ex2 and ex3. removed files .un~

---
+++ ex3.rb
@@ -0,0 +1,27 @@
+# will print out
+puts "I will now count my chickens:"
+
+# #{} evalutes the calculation within string
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+# just calculates
+puts 3 + 2 + 1 -5 +4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 -7?"
+
+# < evalutes to true or false
+puts 3+2<5-7
+
+puts "What is 3+2? #{3+2}"
+puts "What is 5 - 7? #{5-7}"
+
+puts "oh that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or eqaul? #{5 <= -2}"
